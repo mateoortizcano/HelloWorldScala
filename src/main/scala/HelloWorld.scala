2c714a4955d472821ff2ea83c615397a32770460
mateo <maorca@outlook.com>
2018-02-15 14:57:30 -0500

    Algunos métodos haciendo uso de Scala.

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,13 +1,25 @@
 object HelloWorld extends App{
-
-  def sayHello (): String = {
-    println("Hello, World")
-    "Hello, World!"
+  def calcularFactorial(x: Int): Int ={
+    val y: Int = if (x > 1)
+      x * calcularFactorial(x-1)
+    else
+      1
+    y
   }
 
-  def cube(x: Int) = {
-    x * x * x
+  def calcularFibonacci(nroValores: Int, nro1: Int = 0, nro2: Int = 1): Int = {
+    val y: Int = if(nroValores > 1)
+      calcularFibonacci(nroValores-1, nro2, nro1+nro2)
+    else if (nroValores == 1)
+      nro1+nro2
+    else 0
+    y
   }
 
-  sayHello()
+  def suma(x: Int, y: Int): Int = x + y
+
+  def funcion1[A,B](num1: A, num2: A, x: (A, A) => B) =
+    println(s"La suma de los valores es ${x(num1, num2)}")
+
+  funcion1[Int, Int](-32, 3, suma(_,_))
 }
--- src/test/scala/HelloWorldTest.scala
+++ src/test/scala/HelloWorldTest.scala
@@ -1,10 +1,18 @@
 import org.scalatest.FunSuite
 
 class HelloWorldTest extends  FunSuite{
-  test("HelloWorld.cube") {
-    assert(HelloWorld.cube(3) === 27)
+  test("HelloWorld.calcularFactorial") {
+    assert(HelloWorld.calcularFactorial(5) === 120)
+    assert(HelloWorld.calcularFactorial(-1) === 1)
+    assert(HelloWorld.calcularFactorial(0) === 1)
+    assert(HelloWorld.calcularFactorial(10) === 3628800)
   }
-  test("HelloWorld.sayHello"){
-    assert(HelloWorld.sayHello === "Hello, World!")
+
+  test("HelloWorld.calcularFibonacci") {
+    assert(HelloWorld.calcularFibonacci(2) === 2)
+    assert(HelloWorld.calcularFibonacci(5) === 8)
+    assert(HelloWorld.calcularFibonacci(6) === 13)
+    assert(HelloWorld.calcularFibonacci(0) === 0)
+    assert(HelloWorld.calcularFibonacci(-3) === 0)
   }
 }
